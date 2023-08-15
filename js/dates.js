56778461d5a6506db69baf01f1934d6061fa6928
Konstantin <konsi@hdr-it.de>
2023-08-16 00:03:26 +0200

    new formating

--- js/dates.js
+++ js/dates.js
@@ -1,17 +1,23 @@
 const apiUrl = 'https://ffp.hdr-it.de/api/nj';
 
-// Element auswählen, in dem das Start Datum angezeigt wird
-const startDatumElement = document.getElementById('startDatum');
+        // Element auswählen, in dem das Start Datum angezeigt wird
+        const startDatumElement = document.getElementById('startDatum');
 
-// Fetch-Anfrage an die API senden
-fetch(apiUrl)
-    .then(response => response.json()) // Die Antwort als JSON interpretieren
-    .then(data => {
-        // Daten verarbeiten
-        const startDatum = data.startDatum; // Annahme: Die API gibt das Start Datum im 'startDatum' Feld zurück
-        startDatumElement.textContent = `Start Datum: ${startDatum}`;
-    })
-    .catch(error => {
-        console.error('Fehler beim Abrufen der Daten:', error);
-        startDatumElement.textContent = 'Fehler beim Abrufen der Daten';
-    });
+        // Fetch-Anfrage an die API senden
+        fetch(apiUrl)
+            .then(response => response.json()) // Die Antwort als JSON interpretieren
+            .then(data => {
+                // Zeitstempel in ein JavaScript Date-Objekt umwandeln
+                const startDatum = new Date(data.startDatum);
+                
+                // Datum in lesbarer Form anzeigen
+                const options = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', timeZoneName: 'short' };
+                const formattedStartDatum = startDatum.toLocaleDateString('de-DE', options);
+
+                // Anzeige aktualisieren
+                startDatumElement.textContent = `Start Datum: ${formattedStartDatum}`;
+            })
+            .catch(error => {
+                console.error('Fehler beim Abrufen der Daten:', error);
+                startDatumElement.textContent = 'Fehler beim Abrufen der Daten';
+            });
