e3a7cac38d63899c3cdfa97c73f6276248ef05d3
Konstantin <142250002+konsihdr@users.noreply.github.com>
2023-10-06 19:34:19 +0000

    updating api url to google cloud run

--- js/dates.js
+++ js/dates.js
@@ -1,23 +1,23 @@
-const apiUrl = 'https://ffp.hdr-it.de/api/nj';
+const apiUrl = 'https://ffp-api-7qumc7rb4q-ey.a.run.app/api/nj';
 
-        // Element auswählen, in dem das Start Datum angezeigt wird
-        const startDatumElement = document.getElementById('startDatum');
+// Element auswählen, in dem das Start Datum angezeigt wird
+const startDatumElement = document.getElementById('startDatum');
 
-        // Fetch-Anfrage an die API senden
-        fetch(apiUrl)
-            .then(response => response.json()) // Die Antwort als JSON interpretieren
-            .then(data => {
-                // Zeitstempel in ein JavaScript Date-Objekt umwandeln
-                const startDatum = new Date(data.start);
-                
-                // Datum in lesbarer Form anzeigen
-                const options = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', timeZoneName: 'short' };
-                const formattedStartDatum = startDatum.toLocaleDateString('de-DE', options);
+// Fetch-Anfrage an die API senden
+fetch(apiUrl)
+    .then(response => response.json()) // Die Antwort als JSON interpretieren
+    .then(data => {
+        // Zeitstempel in ein JavaScript Date-Objekt umwandeln
+        const startDatum = new Date(data.start);
+        
+        // Datum in lesbarer Form anzeigen
+        const options = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', timeZoneName: 'short' };
+        const formattedStartDatum = startDatum.toLocaleDateString('de-DE', options);
 
-                // Anzeige aktualisieren
-                startDatumElement.textContent = `${formattedStartDatum}`;
-            })
-            .catch(error => {
-                console.error('Fehler beim Abrufen der Daten:', error);
-                startDatumElement.textContent = 'Fehler beim Abrufen der Daten';
-            });
+        // Anzeige aktualisieren
+        startDatumElement.textContent = `${formattedStartDatum}`;
+    })
+    .catch(error => {
+        console.error('Fehler beim Abrufen der Daten:', error);
+        startDatumElement.textContent = 'Fehler beim Abrufen der Daten';
+    });
